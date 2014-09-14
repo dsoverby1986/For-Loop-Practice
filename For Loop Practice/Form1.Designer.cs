04b4df2d668e6a78e90b3cfa48a83794ae42a863
Shane Overby <dsoverby1986@gmail.com>
2014-09-14 14:31:43 -0400

    initial commit

---
+++ For Loop Practice/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace For_Loop_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblLoopStart = new System.Windows.Forms.Label();
+            this.lblLoopEnd = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblMultiplyBy = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Multiply That Shit Bitch!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblLoopStart
+            // 
+            this.lblLoopStart.AutoSize = true;
+            this.lblLoopStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoopStart.Location = new System.Drawing.Point(12, 30);
+            this.lblLoopStart.Name = "lblLoopStart";
+            this.lblLoopStart.Size = new System.Drawing.Size(98, 16);
+            this.lblLoopStart.TabIndex = 3;
+            this.lblLoopStart.Text = "Start Number";
+            // 
+            // lblLoopEnd
+            // 
+            this.lblLoopEnd.AutoSize = true;
+            this.lblLoopEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoopEnd.Location = new System.Drawing.Point(12, 84);
+            this.lblLoopEnd.Name = "lblLoopEnd";
+            this.lblLoopEnd.Size = new System.Drawing.Size(93, 16);
+            this.lblLoopEnd.TabIndex = 4;
+            this.lblLoopEnd.Text = "End Number";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 201);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 121);
+            this.listBox1.TabIndex = 5;
+            // 
+            // lblMultiplyBy
+            // 
+            this.lblMultiplyBy.AutoSize = true;
+            this.lblMultiplyBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplyBy.Location = new System.Drawing.Point(12, 138);
+            this.lblMultiplyBy.Name = "lblMultiplyBy";
+            this.lblMultiplyBy.Size = new System.Drawing.Size(83, 16);
+            this.lblMultiplyBy.TabIndex = 6;
+            this.lblMultiplyBy.Text = "Multiply By";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 334);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblMultiplyBy);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblLoopEnd);
+            this.Controls.Add(this.lblLoopStart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblLoopStart;
+        private System.Windows.Forms.Label lblLoopEnd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblMultiplyBy;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ For Loop Practice/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace For_Loop_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double outputValue = 0;
+            bool isNumber = false;
+
+            isNumber = double.TryParse(textBox1.Text, out outputValue);
+
+            if (!isNumber)
+            {
+                MessageBox.Show("Type numbers in the text boxes you dumb fuck!");
+            }
+            else
+        {
+                listBox1.Items.Clear();
+
+                double loopStart;
+                double loopEnd;
+                double multiplyBy = double.Parse(textBox3.Text);
+
+                loopStart = double.Parse(textBox1.Text);
+                loopEnd =double.Parse(textBox2.Text);
+
+                double answer = 0;
+
+                for (double i = loopStart; i <= loopEnd; i++)
+                {
+                    answer = multiplyBy * i;
+
+                    listBox1.Items.Add(i + " times " + multiplyBy + " = " + answer.ToString());
+                }
+            }
+        }
+    }
+}
